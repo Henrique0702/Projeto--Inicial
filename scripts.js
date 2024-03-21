612f9ac055f013e685456f3b80be46523976c79a
Henrique0702 <Henriquebd08@gmail.com>
2024-03-21 07:45:53 -0300

    Projeto v2

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome")
 var email = document.getElementById("email");
-var Telefone = document.getElementById("Tel");
+var Telefone = document.getElementById("telefone");
 var CEP = document.getElementById("CEP");
-var Logradouro = document.getElementById("Log");
-var Numero = document.getElementById("Num");
-var Bairro = document.getElementById("Bai");
-var Cidade = document.getElementById("Cid");
-var Estado = document.getElementById("Est");
+var Logradouro = document.getElementById("logradouro");
+var Numero = document.getElementById("numero");
+var bairro = document.getElementById("bairro");
+var Cidade = document.getElementById("cidade");
+var Estado = document.getElementById("estado");
 
 
 var saida = document.getElementById("saida-de-dados");
@@ -22,8 +22,16 @@ function alertar(event){
     // if(resultado == 0){
     //     alert("este número é par!");
     // }
+
+    const url = `https://viacep.com.br/ws/${cep.value}/json`;
+    
+    fetch(url)
+    .then(resposta=>resposta.json())
+    .then(dados=>alert(dados.bairro))
+
+
     saida.innerText = "Nome: " + nome.value +
-      "\n Email: " + email.value + "\nTelefone: " + Telefone.value + "\nCEP: " + CEP.value + "\nLogradouro: " + Log.value + "\nNumero: " + Num.value;
+      "\n Email: " + email.value + "\nTelefone: " + telefone.value + "\nCEP: " + CEP.value + "\nLogradouro: " + logradouro.value + "\nNumero: " + numero.value + "\nBairro: " + bairro.value + "\nCidade: " + cidade.value + "\nEstado: " + estado.value;  
 
 
 }
