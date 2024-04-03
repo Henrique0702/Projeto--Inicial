59867215b4d5389a079b7db9ed5b24837f518922
Henrique0702 <Henriquebd08@gmail.com>
2024-04-03 09:43:59 -0300

    Adição do .catch e do .then

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome")
 var email = document.getElementById("email");
-var Telefone = document.getElementById("telefone");
-var CEP = document.getElementById("CEP");
-var Logradouro = document.getElementById("logradouro");
-var Numero = document.getElementById("numero");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
 var bairro = document.getElementById("bairro");
-var Cidade = document.getElementById("cidade");
-var Estado = document.getElementById("estado");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 
 
 var saida = document.getElementById("saida-de-dados");
@@ -26,12 +26,26 @@ function alertar(event){
     const url = `https://viacep.com.br/ws/${cep.value}/json`;
     
     fetch(url)
-    .then(resposta=>resposta.json())
-    .then(dados=>alert(dados.bairro))
+    .then(function(resposta){
+       return resposta.json();
+    })
+    .then(
+      function(dadosDoEndereco){
+        logradouro.value = dadosDoEndereco.logradouro;
+        bairro.value = dadosDoEndereco.bairro;
+        cidade.value = dadosDoEndereco.localidade;
+        estado.value = dadosDoEndereco.uf;
+        complemento.value = dadosDoEndereco.complemento;
+      }
+    )
+    .catch(function(e){
+      alert(e.message());
+    
+    });
 
 
     saida.innerText = "Nome: " + nome.value +
-      "\n Email: " + email.value + "\nTelefone: " + telefone.value + "\nCEP: " + CEP.value + "\nLogradouro: " + logradouro.value + "\nNumero: " + numero.value + "\nBairro: " + bairro.value + "\nCidade: " + cidade.value + "\nEstado: " + estado.value;  
+      "\n Email: " + email.value + "\nTelefone: " + telefone.value + "\nCEP: " + cep.value + "\nLogradouro: " + logradouro.value + "\nNumero: " + numero.value + "\nBairro: " + bairro.value + "\nCidade: " + cidade.value + "\nEstado: " + estado.value;  
 
 
 }
